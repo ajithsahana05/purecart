e7b955406d8128a856dd06729c34f0f41d97eb29
AJITHKUMAR <45867914+ajithsahana05@users.noreply.github.com>
2025-04-30 09:16:44 +0530

    Update settings.py

--- purecart_backend/settings.py
+++ purecart_backend/settings.py
@@ -81,13 +81,9 @@ WSGI_APPLICATION = 'purecart_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'purecart',
-        'USER': 'root',
-        'PASSWORD': 'pass123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
